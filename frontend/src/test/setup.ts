6b1866f78f2c94a35975d91056ea3d87a0016214
Unai <unai.devel@gmail.com>
2026-05-19 14:27:37 +0200

    chore(TMW-0015): add feature translation keys for en/es

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,5 +1,6 @@
 import { cleanup } from "@testing-library/react";
 import { afterAll, afterEach, beforeAll } from "vitest";
+import "@/lib/i18n";
 import { server } from "./server";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
