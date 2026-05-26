369b1b50e361c15788f711912d813f9440b7f79c
Unai <unai.devel@gmail.com>
2026-05-26 17:36:40 +0200

    chore(TMW-0020): bump commitlint scope for new branch

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   prompt: {
-    scopes: ["TMW-0019"],   // Include project scopes
+    scopes: ["TMW-0020"],   // Include project scopes
   },
 };
